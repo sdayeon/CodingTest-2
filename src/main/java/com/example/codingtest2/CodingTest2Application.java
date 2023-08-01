73f02f983a40081cdeee0ec96d029801bddcc6d9
sdayeon <dayeon@blue-core.com>
2023-08-01 17:22:53 +0900

    no message

---
+++ src/main/java/com/example/codingtest2/CodingTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.codingtest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingTest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/codingtest2/CodingTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codingtest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingTest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
