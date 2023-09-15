60b2bc27f3ae51794d6718e8ee5cd0c71e15601c
블루코어 성다연 <dayeon@blue-core.com>
2023-09-15 10:31:41 +0900

    hikari config

---
+++ src/main/java/com/example/codingtest2/config/DataSourceConfig.java
@@ -0,0 +1,17 @@
+package com.example.codingtest2.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration(proxyBeanMethods = false)
+public class DataSourceConfig {
+    @Bean
+    @ConfigurationProperties("spring.datasource.hikari")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
